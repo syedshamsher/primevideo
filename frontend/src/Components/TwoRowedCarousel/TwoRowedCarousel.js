4946bf970ed98eaf3e51d4f34dbf3b86b90ba70f
normandy17 <charlz1717@gmail.com>
2021-02-03 06:51:22 +0530

    custom video player/media redux/media cast&crew

--- frontend/src/Components/TwoRowedCarousel/TwoRowedCarousel.js
+++ frontend/src/Components/TwoRowedCarousel/TwoRowedCarousel.js
@@ -4,10 +4,16 @@ import SwiperCore, {Navigation, Pagination, Autoplay, EffectFade} from 'swiper';
 import './styles.css';
 import './styles.scss';
 import 'swiper/swiper-bundle.css';
+import { useHistory } from 'react-router-dom';
+
 
 export const TwoRowedCarousel = () => {
-
+    const history = useHistory()
     SwiperCore.use([Navigation, Pagination, Autoplay, EffectFade])
+
+    const handleClick=()=>{
+        history.push(`/media/601865e80528f5c0d430fa1e`)
+    }
 
     return (
         <div className="prime">
@@ -31,7 +37,7 @@ export const TwoRowedCarousel = () => {
                 
             </SwiperSlide>
             <SwiperSlide>
-                <img  src="https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/89f512cebace67310ad38609e5dde3e4a07a8ae07d803d64177be6548f04a0e5._UR1920,1080_RI_SX356_FMjpg_.jpg" alt="banner" />
+                <img  src="https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/89f512cebace67310ad38609e5dde3e4a07a8ae07d803d64177be6548f04a0e5._UR1920,1080_RI_SX356_FMjpg_.jpg" alt="banner" onClick={()=>handleClick()} />
             </SwiperSlide>
             <SwiperSlide>
                 <img  src="https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/89f512cebace67310ad38609e5dde3e4a07a8ae07d803d64177be6548f04a0e5._UR1920,1080_RI_SX356_FMjpg_.jpg" alt="banner" />
